f968582cba4bcc47430b3e99447c27a5137671b9
N3Ur0t0x1c <N3Ur0t0x1c@live.com>
2014-01-26 21:14:36 +0545

    added comments

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main()
     scanf("%d", &n);
 
     l1 = (struct lista *) malloc(sizeof(struct lista));
-    temp =l1;
+    temp =l1;	//store address of l1 in temp for resetting the pointer after input.
     for(i=0; i<n; i++)
     {
         printf("\nEnter number: ");
@@ -26,18 +26,18 @@ int main()
         }
         else
         {
-            l1->next = (struct lista *) malloc(sizeof(struct lista));
-            l1 = l1->next;
+            l1->next = (struct lista *) malloc(sizeof(struct lista)); //store the address of next item in l1->next
+            l1 = l1->next; // now pass the same address to l1.
         }
     }
 
     printf("\n\nRESULT:\n\n");
 
-    l1 =temp;
+    l1 =temp;// reverting l1 to its initial value
     while(l1 != NULL)
     {
         printf("%d\t", l1->num);
-        l1 = l1->next;
+        l1 = l1->next;	// set l1 to point to next item.
     }
     return 0;
 
